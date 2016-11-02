0e8ec2d0df819f6a8c66788a0a7a9b923babe39c
Lucas Maystre <lucas@maystre.ch>
2016-11-02 13:45:48 +0100

    `fakedata.py` now produces training and validation data.
    Small changes to the fake data generation script. Now it writes out two files,
    one for testing and the other for validation.

--- utils/fakedata.py
+++ utils/fakedata.py
@@ -25,20 +25,28 @@ def rnn_sequence(ws_in, ws_out, ws_h, initial_state):
 def main(args):
     ws_in = np.random.randn(args.nb_items, args.hidden_size)
     ws_out = np.random.randn(args.hidden_size, args.nb_items)
-    for u in range(args.nb_users):
-        ws_h = np.random.randn(args.hidden_size, args.hidden_size)
-        initial_state = np.zeros(args.hidden_size)
-        seq = rnn_sequence(ws_in, ws_out, ws_h, initial_state)
-        for t in range(args.seq_length):
-            print("{} {} {}".format(u, next(seq), t))
+    train_path = "{}-train.txt".format(args.prefix)
+    valid_path = "{}-valid.txt".format(args.prefix)
+    with open(train_path, "w") as tf, open(valid_path, "w") as vf:
+        for u in range(args.nb_users):
+            ws_h = np.random.randn(args.hidden_size, args.hidden_size)
+            initial_state = np.zeros(args.hidden_size)
+            seq = rnn_sequence(ws_in, ws_out, ws_h, initial_state)
+            for t in range(args.train_seq_length):
+                tf.write("{} {} {}\n".format(u, next(seq), t))
+            offset = args.train_seq_length
+            for t in range(args.valid_seq_length):
+                vf.write("{} {} {}\n".format(u, next(seq), offset + t))
 
 
 def _parse_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--prefix', default="fakedata")
     parser.add_argument('--hidden-size', type=int, default=8)
     parser.add_argument('--nb-users', type=int, default=5)
     parser.add_argument('--nb-items', type=int, default=10)
-    parser.add_argument('--seq-length', type=int, default=128)
+    parser.add_argument('--train-seq-length', type=int, default=128)
+    parser.add_argument('--valid-seq-length', type=int, default=128)
     return parser.parse_args()
 
 
