4c757b607d12f2c7ba075f32885cf066d1994940
Lucas Maystre <lucas@maystre.ch>
2016-11-08 15:13:40 +0100

    Improvements to preprocessing script.
    The script can now process both the BrightKite and last.fm datasets. Most of
    the pre-preprocessing logic is shared.

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
@@ -15,21 +14,19 @@ from scipy.stats import entropy
 
 BK_ENTROPY_CUTOFF = 2.5
 LFM_ENTROPY_CUTOFF = 3.0
+
+MIN_OCCURRENCES = 10
 MIN_VALID_SEQ_LEN = 3
 MAX_VALID_SEQ_LEN = 500
 
 
-def brightkite(path, output_dir):
-    """Preprocess the BrightKite dataset.
+def parse_brightkite(path):
+    """Parse the BrightKite dataset.
 
     This takes as input the file `loc-brightkite_totalCheckins.txt` available
     at the following URL: <https://snap.stanford.edu/data/loc-brightkite.html>.
     """
     # Format: [user] [check-in time] [latitude] [longitude] [location id].
-    # First step: read the data, convert the timestamps, and count the number
-    # of times each item occurs.
-    data = list()
-    occurrences = collections.defaultdict(lambda: 0)
     with open(path) as f:
         for i, line in enumerate(f):
             try:
@@ -40,76 +37,11 @@ def brightkite(path, output_dir):
                 continue
             dt = datetime.datetime.strptime(ts, "%Y-%m-%dT%H:%M:%SZ")
             ts = time.mktime(dt.timetuple())
-            data.append((usr, loc, ts))
-            occurrences[loc] += 1
-    # Second step: prune items that occurred less than 10 times, and assign ids
-    # to them.
-    next_uid = 0
-    next_lid = 0
-    usr2id = dict()
-    loc2id = dict()
-    tmp_dict = collections.defaultdict(list)
-    for usr, loc, ts in data:
-        if occurrences[loc] < 10:
-            continue
-        if usr not in usr2id:
-            usr2id[usr] = next_uid
-            next_uid += 1
-        if loc not in loc2id:
-            loc2id[loc] = next_lid
-            next_lid += 1
-        tmp_dict[usr2id[usr]].append((ts, loc2id[loc]))
-    # Third step: order user sequences.
-    seq_dict = dict()
-    for uid, seq in tmp_dict.items():
-        seq = [loc for ts, loc in sorted(seq)]
-        seq_dict[uid] = seq
-    # Fourth step: split into training and validation step. Ignore users who
-    # consumed less than 10 items or who do not meet entropy requirements.
-    train = dict()
-    valid = dict()
-    for uid, seq in seq_dict.items():
-        if len(seq) <= 10:
-            continue
-        hist = collections.defaultdict(lambda: 0)
-        for lid in seq:
-            hist[lid] += 1
-        if entropy(hist.values()) <= BK_ENTROPY_CUTOFF:
-            continue
-        # Implementation note: round(0.025 * 100) gives 3.0 in Python, but 2.0
-        # in Julia. Beware! Results might differ!
-        cutoff = max(MIN_VALID_SEQ_LEN, int(round(0.025 * len(seq))))
-        train[uid] = seq[:-cutoff]
-        valid[uid] = seq[-cutoff:]
-    # Finally, prune items in the validation sequences that are not found in
-    # the training sequences, and relabel everything.
-    items = set(itertools.chain(*train.values()))
-    users = set(train.keys())
-    new_uid = dict(zip(users, range(len(users))))
-    new_lid = dict(zip(items, range(len(items))))
-    train2 = dict()
-    valid2 = dict()
-    for u in users:
-        train2[new_uid[u]] = tuple(map(lambda x: new_lid[x], train[u]))
-        valid2[new_uid[u]] = tuple(map(lambda x: new_lid[x],
-                filter(lambda x: x in items, valid[u])))
-    # Write out the sequences.
-    train_path = os.path.join(output_dir, "brightkite-train.txt")
-    valid_path = os.path.join(output_dir, "brightkite-valid.txt")
-    with open(train_path, "w") as tf, open(valid_path, "w") as vf:
-        for uid in train2.keys():
-            t = 0
-            for lid in train2[uid]:
-                tf.write("{} {} {}\n".format(uid, lid, t))
-                t += 1
-            for lid in valid2[uid]:
-                vf.write("{} {} {}\n".format(uid, lid, t))
-                t += 1
-    print("Done.")
+            yield (usr, loc, ts)
 
 
-def lastfm(path):
-    """Preprocess the last.fm dataset.
+def parse_lastfm(path):
+    """Parse the last.fm dataset.
 
     This takes as input the file
     `userid-timestamp-artid-artname-traid-traname.tsv` available at the
@@ -117,16 +49,106 @@ def lastfm(path):
     <http://www.dtic.upf.edu/~ocelma/MusicRecommendationDataset/lastfm-1K.html>.
     """
     # Format: [user] [timestamp] [artist ID] [artist] [track ID] [track].
-    pass
+    with open(path) as f:
+        for i, line in enumerate(f):
+            try:
+                usr, ts, aid, artist, tid, track = line.strip().split('\t')
+            except ValueError:
+                print("could not parse line {} ('{}'), ignoring".format(
+                        i, line.strip()))
+                continue
+            dt = datetime.datetime.strptime(ts, "%Y-%m-%dT%H:%M:%SZ")
+            ts = time.mktime(dt.timetuple())
+            yield (usr, aid, ts)
+
+
+def preprocess(stream, output_dir, prefix="processed", min_entropy=0.0):
+    """Preprocess a stream of (user, item, timestamp) triplets.
+
+    The preprocessing roughly includes the following steps:
+
+    - remove items that occur infrequently,
+    - remove users that consume very few items,
+    - remove users who do not consume "diverse enough" items,
+    - separate data into training and validation sets,
+    - make sure that items in the validation sets appear at least once in the
+      training set,
+    - relabel items and users with consecutive integers.
+    """
+    # Step 1: read stream and count number of item occurrences.
+    data = list()
+    occurrences = collections.defaultdict(lambda: 0)
+    for user, item, ts in stream:
+        data.append((user, item, ts))
+        occurrences[item] += 1
+    # Step 2: remove items that occurred infrequently, create user seqs.
+    tmp_dict = collections.defaultdict(list)
+    for user, item, ts in data:
+        if occurrences[item] < MIN_OCCURRENCES:
+            continue
+        tmp_dict[user].append((ts, item))
+    # Step 3: order user sequences by timestamp.
+    seq_dict = dict()
+    for user, seq in tmp_dict.items():
+        seq = [item for ts, item in sorted(seq)]
+        seq_dict[user] = seq
+    # Step 4: split into training and validation sets. Ignore users who
+    # consumed few items or who do not meet entropy requirements.
+    train = dict()
+    valid = dict()
+    for user, seq in seq_dict.items():
+        if len(seq) <= MIN_OCCURRENCES:
+            continue
+        hist = collections.defaultdict(lambda: 0)
+        for item in seq:
+            hist[item] += 1
+        if entropy(hist.values()) <= min_entropy:
+            continue
+        # Implementation note: round(0.025 * 100) gives 3.0 in Python, but 2.0
+        # in Julia. Beware! Results might differ!
+        cutoff = min(MAX_VALID_SEQ_LEN, max(MIN_VALID_SEQ_LEN,
+                                            int(round(0.025 * len(seq)))))
+        train[user] = seq[:-cutoff]
+        valid[user] = seq[-cutoff:]
+    # Step 5: relabel users and items, and remove items that do not appear in
+    # the training sequences.
+    items = set(itertools.chain(*train.values()))
+    users = set(train.keys())
+    user2id = dict(zip(users, range(len(users))))
+    item2id = dict(zip(items, range(len(items))))
+    train2 = dict()
+    valid2 = dict()
+    for user in users:
+        train2[user2id[user]] = tuple(map(lambda x: item2id[x], train[user]))
+        valid2[user2id[user]] = tuple(map(lambda x: item2id[x],
+                filter(lambda x: x in items, valid[user])))
+    # Step 6: write out the sequences.
+    train_path = os.path.join(output_dir, "{}-train.txt".format(prefix))
+    valid_path = os.path.join(output_dir, "{}-valid.txt".format(prefix))
+    with open(train_path, "w") as tf, open(valid_path, "w") as vf:
+        for uid in user2id.values():
+            t = 0
+            for iid in train2[uid]:
+                tf.write("{} {} {}\n".format(uid, iid, t))
+                t += 1
+            for iid in valid2[uid]:
+                vf.write("{} {} {}\n".format(uid, iid, t))
+                t += 1
+    print("Done.")
 
 
 def main(args):
     if args.which == "brightkite":
-        brightkite(args.path, args.output_dir)
+        stream = parse_brightkite(args.path)
+        cutoff = BK_ENTROPY_CUTOFF
     elif args.which == "lastfm":
-        lastfm(args.path, args.output_dir)
+        stream = parse_lastfm(args.path)
+        cutoff = LFM_ENTROPY_CUTOFF
     else:
         raise RuntimeError("unknown dataset?!")
+    preprocess(stream, args.output_dir,
+               prefix=args.which,
+               min_entropy=cutoff)
 
 
 def _parse_args():
